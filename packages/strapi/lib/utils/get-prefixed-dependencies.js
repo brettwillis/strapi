d666ce8fcb1694c7a6dfc53e102fbdf1843da28f
Brett Willis <brettjohnwillis@gmail.com>
2020-12-07 15:21:41 +1300

    Update get-prefixed-dependencies.js

--- packages/strapi/lib/utils/get-prefixed-dependencies.js
+++ packages/strapi/lib/utils/get-prefixed-dependencies.js
@@ -1,5 +1,15 @@
 module.exports = (prefix, pkgJSON) => {
-  return Object.keys(pkgJSON.dependencies)
+  // If in development mode then also look inside "devDependencies"
+  // otherwise only look inside production "dependencies"
+  const dependencies = Object.assign(
+    {},
+    (process.env.NODE_ENV === 'development') 
+      ? pkgJSON.devDependencies 
+      : undefined,
+    pkgJSON.dependencies,
+  );
+
+  return Object.keys(dependencies)
     .filter(d => d.startsWith(prefix) && d.length > prefix.length)
     .map(pkgName => pkgName.substring(prefix.length + 1));
 };
